9414c8652de44551e34bdff75104a46386c5a889
amberlu-design <amberlu12266@gmail.com>
2020-10-19 14:06:13 +0800

    color change

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function fadeInPage() {
 	 var fader = document.getElementById('fader');
     fader.classList.add('fade-out');
 }
+
+function myFunction() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  alert("Copied the text: " + copyText.value);
+}
