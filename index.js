2cad83661934768f3c6878446496b99b705ec979
amberlu-design <amberlu12266@gmail.com>
2020-12-04 22:59:02 +0800

    update

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ function myFunction() {
 	});
 });*/
 
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
 
 
 
